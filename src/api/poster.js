705145e3eb23a8857010ba7930a27a73095501b7
Rick Buitrago <rick@elevatestartup.com>
2020-04-20 13:27:39 -0500

    changed /api/poster to take title instead of id

--- src/api/poster.js
+++ src/api/poster.js
@@ -3,7 +3,20 @@
 const Wreck = require('@hapi/wreck')
 const Joi = require('@hapi/joi')
 
-let posterCall = async (api, id) => {
+let movieCall = async (key, title) => {
+  const { req, res, payload } = await Wreck.get(`http://www.omdbapi.com/?apikey=${key}&t=${title}`)
+  return payload
+}
+
+let posterCall = async (api, title) => {
+  let id
+  try {
+    const movieJson = await movieCall(process.env.API_KEY, title)
+    id = JSON.parse(movieJson)['imdbID']
+  } catch (err) {
+    console.error(err)
+  } 
+  // tried the img.omdbapi.com with the 't' parameter but their text search on that endpoint doesn't work just as fine
   const { req, res, payload } = await Wreck.get(`http://img.omdbapi.com/?apikey=${api}&i=${id}`)
   return payload
 }
@@ -14,18 +27,18 @@ const plugin = {
   register: (server, options) => {
     server.route({
       method: ['GET', 'PUT', 'POST'],
-      path: '/api/poster/{id?}',
+      path: '/api/poster/{title?}',
       config: {
         validate: {
           params: {
-            id: Joi.string().min(9).max(10).required()
+            title: Joi.string().required()
           }
         }
       },
       handler: async (request, h) => {
         let findPoster
         try {
-          findPoster = await posterCall(process.env.API_KEY, request.params.id)
+          findPoster = await posterCall(process.env.API_KEY, request.params.title)
         } catch (err) {
           console.error(err)
         }
